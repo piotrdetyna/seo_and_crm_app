4a22428333202f5c0b0306a1651d57c830b73b95
Piotr Detyna <piotrek.detyna@gmail.com>
2023-08-28 03:03:26 +0200

    improved CSS for external links site

--- base/static/external_links.js
+++ base/static/external_links.js
@@ -2,9 +2,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const findLinksButton = document.querySelector('#find-links');
     const siteId = findLinksButton.value    
     findLinksButton.onclick = () => {
+        findLinksButton.classList.add('disabled')
         let to_exclude = document.querySelector('#to-exclude').value.split(",").map(item => item.trim());
         to_exclude = to_exclude.filter(element => element.trim() !== '');
-        console.log(to_exclude)
         fetch('/find-external/', {
             method: 'PUT',
             headers: {
@@ -19,23 +19,11 @@ document.addEventListener('DOMContentLoaded', () => {
         .then(response => {
             if (response.ok) {
                 response.json().then(data => {
-                    console.log(data)
-                    document.querySelector('#find-links-message').innerHTML = 'Znaleziono linki, możesz odświeżyć stronę'
+                    location.reload()
                 })
-                
+
             }
         })
     }
-    const toggleButton = document.getElementById('toggleButton');
-    const contentDiv = document.getElementById('accordion-item');
-  
-    toggleButton.addEventListener('click', () => {
-      if (contentDiv.style.display === 'none') {
-        contentDiv.style.display = 'block';
-        toggleButton.textContent = 'Ukryj zawartość';
-      } else {
-        contentDiv.style.display = 'none';
-        toggleButton.textContent = 'Pokaż zawartość';
-      }
-    });
+
 })
